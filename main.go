0c6f0e8c675d01b7ae9f2016758d203df9545a25
yushar <yusharw@gmail.com>
2024-04-05 02:07:38 +0700

    [UPDATE] Edited Endpoint and README.md

--- main.go
+++ main.go
@@ -18,16 +18,16 @@ func main() {
 			groupCustomers.GET("/", handlers.ViewDataPelanggan) // /api/customers
 
 			// Menampilkan pelanggan bedasarkan id
-			groupCustomers.GET("/search/:id", handlers.ViewDataPelangganById) // /api/customers/search/:id
+			groupCustomers.GET("/:id", handlers.ViewDataPelangganById) // /api/customers/search/:id
 
 			// Mendaftarkan pelanggan baru
-			groupCustomers.POST("/add", handlers.AddPelanggan) // /api/customers/add
+			groupCustomers.POST("/", handlers.AddPelanggan) // /api/customers/add
 
 			// Update data pelanggan
-			groupCustomers.PUT("/update/:id", handlers.UpdatePelanggan) // /api/customers/update/:id
+			groupCustomers.PUT("/:id", handlers.UpdatePelanggan) // /api/customers/update/:id
 
 			// Hapus data pelanggan
-			groupCustomers.DELETE("/delete/:id", handlers.DeletePelanggan) // /api/customers/delete/:id
+			groupCustomers.DELETE("/:id", handlers.DeletePelanggan) // /api/customers/delete/:id
 		}
 
 		groupServices := groupApi.Group("/services") // /api/services
@@ -36,16 +36,16 @@ func main() {
 			groupServices.GET("/", handlers.ViewListService) // /api/services
 
 			// Menampilkan pelanggan bedasarkan id
-			groupServices.GET("/search/:id", handlers.ViewListServiceById) // /api/services/search/:id
+			groupServices.GET("/:id", handlers.ViewListServiceById) // /api/services/search/:id
 
 			// Mendaftarkan pelanggan baru
-			groupServices.POST("/add", handlers.AddNewService) // /api/services/add
+			groupServices.POST("/", handlers.AddNewService) // /api/services/add
 
 			// Update data pelanggan
-			groupServices.PUT("/update/:id", handlers.UpdateService) // /api/services/update/:id
+			groupServices.PUT("/:id", handlers.UpdateService) // /api/services/update/:id
 
 			// Hapus data pelanggan
-			groupServices.DELETE("/delete/:id", handlers.DeleteService) // /api/services/delete/:id
+			groupServices.DELETE("/:id", handlers.DeleteService) // /api/services/delete/:id
 		}
 
 		groupEmployess := groupApi.Group("/employees") // /api/employees
@@ -54,16 +54,16 @@ func main() {
 			groupEmployess.GET("/", handlers.ViewDataPegawai) // /api/employees
 
 			// Menampilkan pegawai bedasarkan id
-			groupEmployess.GET("/search/:id", handlers.ViewDataPegawaiById) // /api/employees/search/:id
+			groupEmployess.GET("/:id", handlers.ViewDataPegawaiById) // /api/employees/search/:id
 
 			// Mendaftarkan pegawai baru
-			groupEmployess.POST("/add", handlers.AddPegawai) // /api/employees/add
+			groupEmployess.POST("/", handlers.AddPegawai) // /api/employees/add
 
 			// Update data pegawai
-			groupEmployess.PUT("/update/:id", handlers.UpdatePegawai) // /api/employees/update/:id
+			groupEmployess.PUT("/:id", handlers.UpdatePegawai) // /api/employees/update/:id
 
 			// Hapus data pegawai
-			groupEmployess.DELETE("/delete/:id", handlers.DeletePegawai) // /api/employees/delete/:id
+			groupEmployess.DELETE("/:id", handlers.DeletePegawai) // /api/employees/delete/:id
 		}
 
 		groupTransactions := groupApi.Group("/transactions") // /api/transactions
@@ -72,16 +72,16 @@ func main() {
 			groupTransactions.GET("/", handlers.ViewTransaction) // /api/transactions
 
 			// Menampilkan transaksi bedasarkan id transaksi
-			groupTransactions.GET("/search/id/:id", handlers.ViewTransactionByTransactionID) // /api/transactions/search/:id
+			groupTransactions.GET("/:id", handlers.ViewTransactionByTransactionID) // /api/transactions/search/:id
 
 			// Menampilkan transaksi bedasarkan id pelanggan
-			groupTransactions.GET("/search/users/id/:id", handlers.ViewTransactionByCustomerID) // /api/transactions/search/users/id/:id
+			groupTransactions.GET("/customers/id/:id", handlers.ViewTransactionByCustomerID) // /api/transactions/search/users/id/:id
 
 			// Menampilkan transaksi bedasarkan nama pelanggan
-			groupTransactions.GET("/search/users/name/:name", handlers.ViewTransactionByCustomerName) // /api/transactions/search/users/name/:name
+			groupTransactions.GET("/customers/name/:name", handlers.ViewTransactionByCustomerName) // /api/transactions/search/users/name/:name
 
 			// Mendaftarkan transaksi baru
-			groupTransactions.POST("/add", handlers.AddTransaksi) // /api/transactions/add
+			groupTransactions.POST("/", handlers.AddTransaksi) // /api/transactions/add
 		}
 	}
 
