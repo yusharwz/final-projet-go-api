b7ee417f2a9deed1a337c2ccab76b46f2d1a7703
yushar <yusharw@gmail.com>
2024-04-27 03:57:23 +0700

    [UPDATE] Add .env file for credentials data

--- config/config.go
+++ config/config.go
@@ -3,23 +3,14 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "sudiang123"
-	dbname   = "enigma_laundry"
-)
-
 func ConnectDb() (db *sql.DB, err error) {
 
-	var PsqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	db, err = sql.Open("postgres", PsqlInfo)
+	db, err = sql.Open("postgres", os.Getenv("DB"))
 	if err != nil {
 		fmt.Println(err)
 	}
