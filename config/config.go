78609dc4333f3ffd991cfdeff60e01fa5a930570
Yushar Wahidin Zamzam <yusharw@gmail.com>
2025-02-17 20:28:03 +0700

    Refactor database connection string handling for SSL configuration

--- config/config.go
+++ config/config.go
@@ -10,9 +10,15 @@ import (
 
 func ConnectDb() (db *sql.DB, err error) {
 
-	var PsqlInfo = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s&sslcert=%s&sslkey=%s&sslrootcert=%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL_MODE"), os.Getenv("CLIENT_CERT_PATH"), os.Getenv("CLIENT_KEY_PATH"), os.Getenv("SERVER_CA_PATH"))
+	var dsn string
 
-	db, err = sql.Open("postgres", PsqlInfo)
+	if os.Getenv("DB_SSL_MODE") != "disable" {
+		dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s&sslcert=%s&sslkey=%s&sslrootcert=%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL_MODE"), os.Getenv("CLIENT_CERT_PATH"), os.Getenv("CLIENT_KEY_PATH"), os.Getenv("SERVER_CA_PATH"))
+	} else {
+		dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL_MODE"))
+	}
+
+	db, err = sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("gagal melakukan koneksi ke database %v: %v", db, err)
 	}
